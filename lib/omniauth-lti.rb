ba5f309ba0ef6a5135385dac2ab45accc5cb2a04
Xavi Aracil <xavi@xadsolutions.com>
2013-05-31 11:14:32 +0200

    first commit

---
+++ lib/omniauth-lti.rb
@@ -0,0 +1,7 @@
+require "omniauth-lti/version"
+
+module Omniauth
+  module Lti
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth-lti/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Lti
+    VERSION = "0.0.1"
+  end
+end
